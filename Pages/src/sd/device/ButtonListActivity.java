f85307b2cd8287c13da09a3bc4653342d6baed75
Brett Dixon <brdixon@mines.edu>
2012-04-24 23:23:47 -0600

    Added page changes

---
+++ Pages/src/sd/device/ButtonListActivity.java
@@ -0,0 +1,13 @@
+package sd.device;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class ButtonListActivity extends FragmentActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.side_by_side_fragments);
+    }
+}
